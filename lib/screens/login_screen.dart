53824947e2b751c9131f39529d7b3523c9c79c7a
Ketan <ketantoo@gmail.com>
2020-10-01 11:47:32 +0530

    Initial commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,116 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Image(
+                height: MediaQuery.of(context).size.height / 2.5,
+                width: double.infinity,
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/login_background.jpg"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "FRENZY",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 34,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_box),
+                      hintText: "Username",
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: "Password",
+                      fillColor: Colors.white,
+                      filled: true),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 130,
+                    vertical: 20,
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              Expanded(child: Container()),
+              Container(
+                height: 80,
+                width: double.infinity,
+                color: Theme.of(context).primaryColor,
+                child: Center(
+                  child: Text(
+                    "Don\'t have an account, Sign up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/**
+ * 
+ * 
+ * 
+ */
